3aef7b3d04be3263f7f7e043996d3dd0087cdb13
sevgiabaysal <sevgi.asl@edu.312school.com>
2026-09-01 16:12:16 -0500

    [update][iam] grant terraform-infra-v2 CI access  Path: /iam

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -8,6 +8,11 @@ output "apply_role_arn" {
   value       = aws_iam_role.apply.arn
 }
 
+output "infra_v2_apply_role_arn" {
+  description = "Set as AWS_ROLE_ARN_APPLY in terraform-infra-v2's GitHub environments."
+  value       = aws_iam_role.infra_v2_apply.arn
+}
+
 output "engineer_boundary_policy_name" {
   description = "Pass to ../identity as engineer_boundary_policy_name."
   value       = aws_iam_policy.engineer_boundary.name
